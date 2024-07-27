5044e0439049b4ea8285721a3fde93f1b165cb81
José Edson MBS <joseedsonmbs04@gmail.com>
2024-07-27 15:58:11 -0300

    Pequenas mudanças

--- src/boxbuster/Filmes.java
+++ src/boxbuster/Filmes.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package boxbuster;
 
-/**
- *
- * @author Adm
- */
 public class Filmes extends Produtos{
     private String genero;
     private String diretor;
