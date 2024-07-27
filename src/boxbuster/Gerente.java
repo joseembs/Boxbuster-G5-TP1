a066bfa6ed16452efa198429bade0f97b019e70d
Elis Rodrigues <52414747+elisrb@users.noreply.github.com>
2024-07-27 14:45:01 -0300

    Create Gerente.java

---
+++ src/boxbuster/Gerente.java
@@ -0,0 +1,66 @@
+package boxbuster;
+/**
+ *
+ * @author elisrb
+ */
+public class Gerente extends Funcionario {
+    protected Caixa[] caixas;
+    private int index;
+
+    public Gerente(/*Caixa[] caixas,*/ String senha, int codigoFunc, String nome, int cpf, int idade) {
+        super(senha, codigoFunc, nome, cpf, idade);
+        //this.caixas = caixas;
+        this.index = 0;
+    }
+
+    public Caixa[] getCaixas() {
+        return caixas;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public int getCodigoFunc() {
+        return codigoFunc;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setCaixas(Caixa caixa) {
+        this.caixas[index] = caixa;
+        this.index++;
+        caixa.setGerente(this);
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setCodigoFunc(int codigoFunc) {
+        this.codigoFunc = codigoFunc;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+      
+}
