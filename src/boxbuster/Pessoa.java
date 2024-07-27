8bcf161845cbd9049f1ebe736227aebc42e5e055
Elis Rodrigues <52414747+elisrb@users.noreply.github.com>
2024-07-27 14:40:41 -0300

    Create Pessoa.java

---
+++ src/boxbuster/Pessoa.java
@@ -0,0 +1,17 @@
+package boxbuster;
+
+/**
+ *
+ * @author elisrb
+ */
+public class Pessoa {
+    String nome;
+    int cpf;
+    int idade;
+
+    public Pessoa(String nome, int cpf, int idade) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+}
