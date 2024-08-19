6fbc7229715f6ca403cf82f1ebad74f547d1885a
Henrique Sales <144968260+hsaless@users.noreply.github.com>
2024-08-18 23:26:54 -0300

    + toString()

--- src/boxbuster/Visitante.java
+++ src/boxbuster/Visitante.java
@@ -73,11 +73,18 @@ public class Visitante extends Cliente {
     public void calculaDivida() {
         double totalDivida = 0.0;
         ArrayList<Alugar> alugados = getAlugados();
-
+ 
         for (int i = 0; i < alugados.size(); i++) {
             totalDivida += alugados.get(i).getProduto().getPreco();
         }
 
         setDivida(totalDivida);
     }
+
+    @Override
+    public String toString() {
+        return "Visitante " + super.toString();
+    }
+    
+    
 }
