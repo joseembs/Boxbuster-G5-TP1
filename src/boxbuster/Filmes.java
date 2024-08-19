0dda4a3887511da9cbb8803aeb98a1b5c2706988
Henrique Sales <144968260+hsaless@users.noreply.github.com>
2024-08-19 00:56:23 -0300

    + toString()

--- src/boxbuster/Filmes.java
+++ src/boxbuster/Filmes.java
@@ -6,16 +6,14 @@ package boxbuster;
  */
 public class Filmes extends Produtos{
     private String genero;
-    private String estudio;
     private String diretor;
     
     public Filmes() {
     }
 
-    public Filmes(String nomeProd, double preco, int ano, int codigoProd, int faixaEtaria, boolean alugado, String genero, String estudio, String diretor) {
+    public Filmes(String nomeProd, double preco, int ano, int codigoProd, int faixaEtaria, boolean alugado, String genero, String diretor) {
         super(nomeProd, preco, ano, codigoProd, faixaEtaria, alugado);
         this.genero = genero;
-        this.estudio = estudio;
         this.diretor = diretor;
     }
 
@@ -27,20 +25,17 @@ public class Filmes extends Produtos{
         this.genero = genero;
     }
 
-    public String getEstudio() {
-        return estudio;
-    }
-
-    public void setEstudio(String estudio) {
-        this.estudio = estudio;
-    }
-
     public String getDiretor() {
         return diretor;
     }
 
     public void setDiretor(String diretor) {
         this.diretor = diretor;
+    }
+    
+    @Override
+    public String toString() {
+        return "Filme " + super.toString() + " " + genero + " " + diretor;
     }
     
 }
