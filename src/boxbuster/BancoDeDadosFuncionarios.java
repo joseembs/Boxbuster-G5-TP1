432031951a618a77da990332cfa4e7c9c1dc0d60
elisrb <elisrb2004@gmail.com>
2024-09-01 11:35:22 -0300

    funções novas

--- src/boxbuster/BancoDeDadosFuncionarios.java
+++ src/boxbuster/BancoDeDadosFuncionarios.java
@@ -18,17 +18,25 @@ import java.util.ArrayList;
  */
 public class BancoDeDadosFuncionarios implements BancoDeDados{
     private static String arquivo;
-
+    private static Funcionario funcAtual;
+    
     public BancoDeDadosFuncionarios(String arquivo) {
         this.arquivo = arquivo;
     }
 
+    /*public static Funcionario getFunc_atual() {
+        return funcAtual;
+    }
+
+    public static void setFunc_atual(Funcionario func_atual) {
+        BancoDeDadosFuncionarios.funcAtual = func_atual;
+    }*/
+    
     public void adicionarPessoa(Funcionario func) {
         try (FileWriter fw = new FileWriter(arquivo, true);
              BufferedWriter bw = new BufferedWriter(fw);
              PrintWriter out = new PrintWriter(bw)) {
             out.println(func.toString());
-            out.println("-------------------------------------------"); 
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -77,6 +85,64 @@ public class BancoDeDadosFuncionarios implements BancoDeDados{
         //}
         
         return cliente;
+    }
+    
+    public void removerPessoa(String CPF) {
+        ArrayList<String> linhas = new ArrayList<>();
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
+            String linha;
+            boolean encontrou = false;
+
+            while ((linha = br.readLine()) != null) {
+                if (linha.contains(CPF)) {
+                    encontrou = true;
+                    /*while (!linha.equals("-------------------------------------------")) {
+                        linha = br.readLine(); 
+                    }
+                    linha = br.readLine();*/
+                    
+                } else {
+                    linhas.add(linha); 
+                }
+            }
+
+            if (!encontrou) {
+                System.out.println("Funcionario não encontrado.");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(arquivo))) {
+            for (String linha : linhas) {
+                bw.write(linha);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    public Gerente buscarGerente(String nome) {
+        String[] palavras;
+        try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                palavras = linha.split(" ");
+                if(palavras[0].equals(nome) && palavras[5].equals("Gerente")){
+                    return new Gerente(nome);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return new Gerente("--");
     }
     
     @Override
